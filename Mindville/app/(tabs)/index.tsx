7993e3dd8b127128028d987ba61c3d6d5fd8e0c1
Jeffrey Qin <jeffreyqin612@gmail.com>
2024-05-23 23:36:33 -0400

    added timer function

--- Mindville/app/(tabs)/index.tsx
+++ Mindville/app/(tabs)/index.tsx
@@ -1,53 +1,101 @@
-import { Image, StyleSheet, Platform, View, TouchableOpacity, Dimensions } from 'react-native';
-import React, { useState } from 'react';
+import { Image, StyleSheet, Platform, View, TouchableOpacity, Dimensions, Text } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   const [timerPressed, setTimerPressed] = useState(false);
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef(null);
 
-  const handleTimerPressed = () => {
-    setTimerPressed(!timerPressed)
-  }
+  useEffect(() => {
+    if (timerPressed) {
+      intervalRef.current = setInterval(() => {
+        setTime(currTime => currTime + 1000);
+      }, 1000);
+    } else {
+      clearInterval(intervalRef.current);
+    }
+
+    return () => clearInterval(intervalRef.current);
+  }, [timerPressed]);
+
   
+  const handleTimerPressed = () => {
+    setTimerPressed(!timerPressed);
+
+    if (timerPressed) {
+      setTime(0);
+    }
+  }
+
   return (
-    <View>
-      <TouchableOpacity style={styles.button} onPress={handleTimerPressed}>
-        <Image 
+    <View style={styles.page}>
+      <TouchableOpacity onPress={handleTimerPressed}>
+        <Image
           source={timerPressed? require('@/assets/img/ui/timer_pause.png') : require('@/assets/img/ui/timer_start.png')}
           style={{ width: 200, height: 200 }}
         />
       </TouchableOpacity>
+      <Text> </Text>
+      <Text style={styles.timerCount}>
+        {formatTime(time)}
+      </Text>
     </View>
   );
 }
 
+
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  button: {
+  page: {
     alignItems: 'center',
     justifyContent: 'center',
     width: width,
     height: height
   },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  timerCount: {
+    fontSize: 24,
+    marginTop: 20
+  }
 });
+
+
+const formatTime = (time) => {
+
+  let sec = Math.floor((time / 1000) % 60);
+  let min = Math.floor((time / (1000 * 60)) % 60);
+  let hour = Math.floor((time / (1000 * 60 * 60)) % 24);
+  
+  let timeStr = '';
+
+  if (hour == 0) {
+    timeStr += '00';
+  } else if (hour < 10) {
+    timeStr += '0' + hour.toString();
+  } else {
+    timeStr += hour.toString();
+  } 
+
+  timeStr += ':';
+
+  if (min == 0) {
+    timeStr += '00';
+  } else if (min < 10) {
+    timeStr += '0' + min.toString();
+  } else {
+    timeStr += min.toString();
+  }
+  
+  timeStr += ':';
+
+  if (sec == 0) {
+    timeStr += '00';
+  } else if (sec < 10) {
+    timeStr += '0' + sec.toString();
+  } else {
+    timeStr += sec.toString();
+  }
+
+  return timeStr;
+}
